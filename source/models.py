10d178a264559705406c21ce14a3df91f76f6e1e
Anton Burenko <burenko@incoretex.de>
2022-07-18 00:47:14 +0200

    Add base for sql

---
+++ source/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from source.database import Base
+
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user_chat.id"))
+    title = Column(String)
+    current_page = Column(Integer, default=True)
+    total_pages = Column(Integer, default=True)
+
+    items = relationship("UserChat", back_populates="owner")
+
+
+class UserChat(Base):
+    __tablename__ = "user_chat"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    name = Column(String, index=True)
+    surname = Column(String, index=True)
+    creation_ts = Column(String, )
+
+    owner = relationship("Book", back_populates="items")
